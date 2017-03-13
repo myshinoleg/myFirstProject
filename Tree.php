5221797b60c169b6caf74dfa207d02f2d2ee0938
myshinoleg <myshinoleg@gmail.com>
2017-03-13 08:27:23 +0300

    correct tree

--- Tree.php
+++ Tree.php
@@ -6,14 +6,17 @@ class BinaryNode
     public $left;     // левый потомок типа BinaryNode
     public $right;     // правый потомок типа BinaryNode
 
-    public function __construct($item) {
+
+    public function __construct($item)
+    {
         $this->value = $item;
         // новые потомки - вершина
         $this->left = null;
         $this->right = null;
     }
 
-    public function dump() {
+    public function dump()
+    {
         if ($this->left !== null) {
             $this->left->dump();
         }
@@ -22,24 +25,43 @@ class BinaryNode
             $this->right->dump();
         }
     }
+
+
 }
 
 class BinaryTree
 {
     protected $root; // корень дерева
+    public $connection;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->root = null;
+
+        $username = "root";
+        $password = "0000";
+        $this->connection = new PDO('mysql: host=localhost; dbname=forest', $username, $password);
+
     }
 
-    public function isEmpty() {
+    public function saveInDb($parent, $value)
+    {
+        $this->connection->query("INSERT INTO tree (parent_id, value) VALUE ('$parent', '$value')");
+
+    }
+    public function isEmpty()
+    {
+        echo "isEmpty \n";
         return $this->root === null;
     }
 
-    public function insert($item) {
+    public function insert($item)
+    {
+        echo "insert $item \n";
         $node = new BinaryNode($item);
         if ($this->isEmpty()) {
             $this->root = $node;
+            $s = $this->saveInDb(0, $this->root->value);
         }
         else {
             $this->insertNode($node, $this->root);
@@ -48,18 +70,30 @@ class BinaryTree
 
     protected function insertNode($node, &$subtree)
     {
+        echo "insertNode $node->value, $subtree->value \n";
         if ($subtree === null) {
             $subtree = $node;
+//var_dump($node->value);
         } else {
             if ($node->value > $subtree->value) {
                 $this->insertNode($node, $subtree->right);
+//                $this->saveInDb($node->value, $subtree->value);
+
             } else if ($node->value < $subtree->value) {
                 $this->insertNode($node, $subtree->left);
+//                $this->saveInDb($node->value, $subtree->value);
+            } else {
+
             }
+//            $this->saveInDb($node->value, $subtree->value);
+//
+//            var_dump($node->value, $subtree->value);
+//            echo "\n";
         }
     }
 
-    public function traverse() {
+    public function traverse()
+    {
         // отображение дерева в возрастающем порядке от корня
         $this->root->dump();
     }
@@ -68,12 +102,21 @@ class BinaryTree
 
 $tree = new BinaryTree();
 $tree->insert(4);
-$tree->insert(2);
-$tree->insert(3);
+$tree->insert(6);
+$tree->insert(7);
 $tree->insert(8);
-$tree->insert(5);
+$tree->insert(9);
+$tree->insert(10);
+$tree->insert(11);
+$tree->insert(12);
+$tree->insert(13);
+
+
+//$tree->insert(5);
 $tree->traverse();
 
+
+//var_dump($tree);
 /**
  * int(2)
 int(3)
